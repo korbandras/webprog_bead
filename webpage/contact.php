34d7f1dc88a29c4bbeec2713eaeaec16b45899f6
korbandras <korbandras@gmail.com>
2023-01-17 09:28:33 +0100

    v4

--- webpage/contact.php
+++ webpage/contact.php
@@ -6,28 +6,17 @@
 
 <section>
     <h2>Contact us</h2>
-<?php
-if(isset($_POST["sendmail"])){
-    echo '<p class = "success">Sent successfully</p>';
-    //echo '<p>Name: '.$_POST["name"].'</p>';
-    //echo '<p>Email: '.$_POST["email"].'</p>';
-    //echo '<p>Review: '.$_POST["review"].'</p>';
-}
-else{
-    echo '<p class = "fail">Failed to send</p>';
-}
-?>
     <form method="post" action="contact.php">
         <div>
-            <label for = "inputname">Your name</label>
-            <input type="text" name="name" id="inputname" maxlength="200">
+            <label for = "inputname"><b>Your name</b></label>
+            <input type="text" name="name" id="inputname" maxlength="200" placeholder="Name">
         </div>
         <div>
-            <label for = "inputemail">Your email</label>
-            <input type="email" name="email" id="inputemail" maxlength="250">
+            <label for = "inputemail"><b>Your email</b></label>
+            <input type="email" name="email" id="inputemail" maxlength="250" placeholder="Email">
         </div>
         <div>
-            <label for = "inputreview">Please select your review grade</label>
+            <label for = "inputreview"><b>Please select your review grade</b></label>
             <select name="review" id="inputreview">
                 <option value="Sehr gut">Sehr gut</option>
                 <option value="Gut">Gut</option>
@@ -40,6 +29,17 @@ else{
             <button name="sendmail" type="submit">Send</button>
         </div>
     </form>
+    <?php
+    if(isset($_POST["sendmail"])){
+        echo '<script>alert("Successfully sent a '.$_POST["review"].' review of the site")</script>';
+        //echo '<p>Name: '.$_POST["name"].'</p>';
+        //echo '<p>Email: '.$_POST["email"].'</p>';
+        //echo '<p>Review: '.$_POST["review"].'</p>';
+    }
+    /*else if ($_POST["sendmail"] === null){
+        echo '<script>alert("Failed to send")</script>';
+    }*/
+    ?>
 </section>
 
 <?php include "include/aside.php"; ?>
